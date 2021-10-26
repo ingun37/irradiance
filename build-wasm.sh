ff7c8ae3eedf3202b54a3ecf09f7dad8891ec1cc
Ingun Jon <ingun37@gmail.com>
2021-10-26 23:54:38 +0900

    wip

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,2 +1,2 @@
 mkdir wasm-out
-ahc-link --input-hs Wasm.hs --browser --export-function=rotatedHemisphere --output-directory wasm-out --ghc-option -i../src
+ahc-link --input-hs Wasm.hs --browser --export-function=rotatedHemisphere --output-directory wasm-out --ghc-option -isrc
