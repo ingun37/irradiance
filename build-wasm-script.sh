969868b1fa8f2593ec76934273dfc69ae94c50c7
Ingun Jon <ingun37@gmail.com>
2021-11-04 22:18:36 +0900

    export function convertToPngCubemap

--- build-wasm-script.sh
+++ build-wasm-script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir wasm-out
-ahc-link --input-hs Wasm.hs --browser --export-function=rotatedHemisphere --output-directory wasm-out --ghc-option -isrc
+ahc-link --input-hs Wasm.hs --browser --export-function=rotatedHemisphere --export-function=convertToPngCubeMap --output-directory wasm-out --ghc-option -isrc
